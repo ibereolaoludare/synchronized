87b7be20484d76724ca3ee5044bf2c9060faefa5
Ibereola Oludare <96877735+ibereolaoludare@users.noreply.github.com>
2025-02-16 23:06:42 +0100

    Fixed stuff

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,14 +5,6 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-interface CartItem {
-  title: string;
-  price: number;
-  size: string;
-  id: number;
-  quantity: number;
-}
-
 export const updateCartData = () => {
   window.dispatchEvent(new Event("cart-data-updated")); // Manually trigger event
 };
