e5e72b18350149ccc51dea5b6a11775ea564d10b
Ibereola Oludare <96877735+ibereolaoludare@users.noreply.github.com>
2025-02-21 21:20:07 +0100

    Update ErrorPage.tsx

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -18,18 +18,20 @@ export default function ErrorPage() {
                     value={404}
                     className="text-7xl text-white"
                 />
-                <div className="w-4/6 flex flex-col items-center justify-center gap-1">
-                    <TypingAnimation className="text-sm font-normal text-center">Woah, how did we get here?</TypingAnimation>
-                    <TextAnimate className="text-[.65rem] text-slate-200 text-center">
+                <div className="sm:w-4/6 flex flex-col items-center justify-center gap-2">
+                    <TypingAnimation className="text-sm font-normal text-center">
+                        Woah, how did we get here?
+                    </TypingAnimation>
+                    <TextAnimate className="text-[.65rem] w-4/6 text-slate-200 text-center">
                         If this persists we might not be synchronized this time.
                     </TextAnimate>
-                    <Link to={"/"}>
-                        <Button className="uppercase text-xs w-min bg-white border-none rounded-none text-black hover:bg-white/80">
-                            Go to Homepage
-                            <ArrowRight />
-                        </Button>
-                    </Link>
                 </div>
+                <Link to={"/"}>
+                    <Button className="uppercase text-xs w-min bg-white border-none rounded-none text-black hover:bg-white/80">
+                        Go to Homepage
+                        <ArrowRight />
+                    </Button>
+                </Link>
             </div>
         </div>
     );
