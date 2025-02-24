6a27fd62bf0a566168208362705445657229e99b
Ibereola Oludare <96877735+ibereolaoludare@users.noreply.github.com>
2025-02-24 19:16:44 +0100

    Fix

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 import { Database } from "./supabase-types";
 
-const supabaseUrl: string = import.meta.env.SUPABASE_URL;
-const supabaseKey: string = import.meta.env.SUPABASE_ANON_KEY;
+const supabaseUrl: string = import.meta.env.VITE_PUBLIC_SUPABASE_URL;
+const supabaseKey: string = import.meta.env.VITE_PUBLIC_SUPABASE_ANON_KEY;
 
 const supabase = createClient<Database>(supabaseUrl, supabaseKey);
 
