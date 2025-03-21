dfa806b73a0e89ce8badf0b981969fc6b0e91473
Ibereola Oludare <96877735+ibereolaoludare@users.noreply.github.com>
2025-03-21 19:33:48 +0100

    Update HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -41,6 +41,7 @@ export default function HomePage() {
 
     useEffect(() => {
         checkCategory();
+        filterCategories("");
     }, []);
 
     const [searchQuery, setSearchQuery] = useState("");
